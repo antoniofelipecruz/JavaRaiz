12a9606bc075a6fc67993c85c2a11230545f0c78
King Antonio <hbmhad@outlook.com>
2025-04-08 17:58:47 -0500

    GI2

--- voluntario.java
+++ voluntario.java
@@ -3,6 +3,12 @@ public class voluntario{
 		System.out.println(1+"1");
 		System.out.println("Antônio");
 		System.out.printf("Eu tenho %d anos e vou ver o brasil ganhar a copa%n",20);
+		int n1;
+		double n2;
+		n1 = 2;
+		n2 = 9.9*n1;
+		int n3 = 3*n1;
+		System.out.println(n3);
 	}
 
 }
